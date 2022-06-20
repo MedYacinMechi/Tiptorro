112ce156cabb720d7c5b4fa435f0243f92b0f035
Mohammed Yacin Mechi <87264001+MedYacinMechi@users.noreply.github.com>
2022-06-20 04:11:23 +0100

    Update PrivateRoute.js

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-
-const auth = localStorage.getItem("accessToken");
+import { Route } from "react-router-dom";
 
 const PrivateRoute = ({ exact, component: Component, ...rest }) => (
   <Route
     exact={exact ? true : false}
     rest
-    render={(props) =>
-      auth ? (
-        <Component {...props} {...rest}></Component>
-      ) : (
-        <Redirect to={`${process.env.PUBLIC_URL}/auth-login`}></Redirect>
-      )
-    }
+    render={(props) => <Component {...props} {...rest}></Component>}
   ></Route>
 );
 
